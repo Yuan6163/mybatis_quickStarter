57842d2764ac0ec25fab068bbc7fe7e3f9b7c4fd
Yuan <1298016163@qq.com>
2021-12-18 22:07:40 +0800

    1.添加核心配置文件

---
+++ src/main/java/com/mypro/test/MybatisTest.java
@@ -0,0 +1,32 @@
+package com.mypro.test;
+
+import com.mypro.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @author ColdCurrent
+ * @Package com.mypro.test
+ * @Description:
+ * @date 2021/12/18 21:06
+ */
+public class MybatisTest {
+    @Test
+    public void test1() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sessionFactory.openSession();
+        List<User> users = sqlSession.selectList("user.findAll");
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+    }
+}
